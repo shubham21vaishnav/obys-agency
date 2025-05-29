5b83a28600780eadd91714a5a3dc52a155958c7c
shubham vaishnav <shubhamv2108@gmail.com>
2025-05-29 22:07:01 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,45 +1,73 @@
-var time = gsap.timeline()
+function loadingAnimation() {
+    var tl = gsap.timeline();
+
+    tl.from('.line h1', {
+        y: 200,
+        stagger: 0.25,
+        duration: 0.6,
+    })
+    tl.from(".line h4", {
+        y: 150,
+        stagger: 0.25,
+        duration: 0.6,
+
+    })
+    tl.from("#line-part1", {
+        opacity: 0,
+        onStart: function () {
+            var h5timer = document.querySelector("#line-part1 h2");
+            var grow = 0;
+            setInterval(function () {
+                if (grow < 100) {
+                    h5timer.innerHTML = grow++;
+                } else {
+                    h5timer.innerHTML = grow;
+                }
+            }, 27);
+        },
+    });
+
+    tl.to(".line h4", {
+        animationName: "anime",
+        opacity: 1,
+    })
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.4,
+        delay: 4,
+        display: "none"
+    })
+    tl.from("#page1", {
+        delay: 0.2,
+        y: 1600,
+        opacity: 0,
+        // ease:power4
+    })
+
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+        y:120,
+        stagger: 0.2,
+    })
+
+}
+function cursoranimation() {
+    document.addEventListener("mousemove", function (dets) {
+
+        gsap.to("#crsr", {
+            left: dets.x,
+            top: dets.y
+        })
+    })
+
+    Shery.makeMagnet("#nav-part2 h4", {
+
+    });
+
+}
 
 
-time.from("#line-part1", {
-    opacity: 0,
-    delay: 0.5,
-    onstart: function () {
-        var h2timer = document.querySelector('#line-part1 h2')
-        var inc = 0
-        setInterval(function(){
-            if (inc<100) {
-                h2timer.innerHTML = inc++
-            } else {
-                h2timer.innerHTML = inc
-            }
-
-        },35)
-    }
-
-})
 
 
-time.to(".line h4",{
-    animationName:"anime",
-    opacity:1,
-})
 
-time.from('.line h1', {
-    y: 200,
-    stagger: 0.25,
-    duration: 0.6,
-})
-
-time.to("#loader", {
-    opacity: 0,
-    duration: 0.4,
-    delay: 4,
-    display:"none"
-})
-time.from("#page1",{
-    delay:0.2,
-    y:1600,
-    opacity:0,
-    ease:power4
-})
+loadingAnimation();
+cursoranimation();
